28f06c14a5564f8ed27199948b4ccb1afc90c538
romipaz <eccoromi@gmail.com>
2020-05-14 20:52:40 +0200

    select DOM elements

--- js/app.js
+++ js/app.js
@@ -1,4 +1,14 @@
 'use strict';
 document.addEventListener('DOMContentLoaded', () => {
-    //code here
+    //code below here
+    const grid = document.querySelector('.grid');
+
+    //The Array.from() method returns an Array object from any object with a length property or an iterable object.
+    //make an array of indexed squares
+    let squares = Array.from(document.querySelectorAll('.grid div'));
+    
+    const ScoreDisplay = document.querySelector('#score');
+    const StartBtn = document.querySelector('#start-btn');
+    const width = 10;
+    //code above
 })
